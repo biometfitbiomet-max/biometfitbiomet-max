168f6ee18436524c07afbe166cdcd5922a02f957
Forsitex <altnft2022x@gmail.com>
2026-06-21 11:31:38 +0300

    Fix redirect from home to login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,5 @@
-'use client';
-
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push('/login');
-  }, [router]);
-
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-900">
-      <div className="text-white">Redirecting to login...</div>
-    </div>
-  );
+  redirect('/login');
 }
